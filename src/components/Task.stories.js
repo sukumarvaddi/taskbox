5c3c9c88258de826c0d6985b756919e8fb2a7f92
Sukumar Vaddi <svaddi@findhelp.com>
2022-11-17 22:05:19 -0600

    first component story

---
+++ src/components/Task.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Task from "./Task";
+
+export default {
+  component: Task,
+  title: "Task",
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
+
+export const ArchivedAndPinned = Template.bind({});
+
+ArchivedAndPinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED_PINNED",
+  },
+};
